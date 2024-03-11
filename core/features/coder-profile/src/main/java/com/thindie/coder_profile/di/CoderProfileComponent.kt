681eb8e65d5bfafe1ce1938d2bf92618732abcd1
Alex <thindie1@mail.ru>
2024-03-11 12:43:32 +0200

    feature-coders: refactor : add ScopeAnnotation into missing places

--- core/features/coder-profile/src/main/java/com/thindie/coder_profile/di/CoderProfileComponent.kt
+++ core/features/coder-profile/src/main/java/com/thindie/coder_profile/di/CoderProfileComponent.kt
@@ -3,7 +3,6 @@ package com.thindie.coder_profile.di
 
 import androidx.lifecycle.ViewModelProvider
 import com.thindie.coder_profile.di.viewmodels_factory.ViewModelFactoryModule
-import com.thindie.coders.di.DaggerCodersMainComponent
 import com.thindie.common.DependenciesProvider
 import dagger.Component
 
@@ -16,7 +15,7 @@ interface CoderProfileComponent {
     companion object {
         fun init(dependenciesProvider: DependenciesProvider): CoderProfileComponent {
 
-            return DaggerCodersMainComponent.factory()
+            return DaggerCoderProfileComponent.factory()
                 .create(dependenciesProvider)
         }
     }
