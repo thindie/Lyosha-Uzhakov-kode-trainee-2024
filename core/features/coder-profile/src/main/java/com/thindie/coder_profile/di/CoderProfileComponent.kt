a72f9e6f48319db8383265b90794092ca65c252a
Alex <thindie1@mail.ru>
2024-03-11 12:30:07 +0200

    app module: due complex routing task implementation:
    add: coder profile module, update buildSrc, update project inclusions

---
+++ core/features/coder-profile/src/main/java/com/thindie/coder_profile/di/CoderProfileComponent.kt
@@ -0,0 +1,31 @@
+package com.thindie.coder_profile.di
+
+
+import androidx.lifecycle.ViewModelProvider
+import com.thindie.coder_profile.di.viewmodels_factory.ViewModelFactoryModule
+import com.thindie.coders.di.DaggerCodersMainComponent
+import com.thindie.common.DependenciesProvider
+import dagger.Component
+
+
+@CoderProfileScope
+@Component(
+    dependencies = [DependenciesProvider::class],modules = [ViewModelFactoryModule::class, ViewModelModule::class]
+)
+interface CoderProfileComponent {
+    companion object {
+        fun init(dependenciesProvider: DependenciesProvider): CoderProfileComponent {
+
+            return DaggerCodersMainComponent.factory()
+                .create(dependenciesProvider)
+        }
+    }
+
+    @Component.Factory
+    interface Factory {
+        fun create(dependenciesProvider: DependenciesProvider): CoderProfileComponent
+    }
+
+    fun provideFactory(): ViewModelProvider.Factory
+
+}
