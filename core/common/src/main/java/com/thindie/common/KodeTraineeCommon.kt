98a590c7a49500ddf0e5747568c1004742c82972
Alex <thindie1@mail.ru>
2024-03-13 22:44:05 +0200

    common: on navigation management update: add args support

--- core/common/src/main/java/com/thindie/common/KodeTraineeCommon.kt
+++ core/common/src/main/java/com/thindie/common/KodeTraineeCommon.kt
@@ -6,7 +6,17 @@ object KodeTraineeCommon {
     }
 
     object FeatureDestinations {
+        private const val profile = "profile/"
+        object Arguments {
+            const val id = "userId"
+        }
+
+
         const val codersSummary = "coders"
-        const val coderProfile = "profile"
+        const val coderProfile = "$profile{${Arguments.id}}"
+
+        fun argumentCoderProfile(givenId: String): String {
+           return "$profile$givenId"
+        }
     }
 }
