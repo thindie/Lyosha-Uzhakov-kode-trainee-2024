d0adfdde18e0f53b3e23a431fc1228daaae51e8a
Alex <thindie1@mail.ru>
2024-03-13 11:56:06 +0200

    feature coders: add naive screen implementation, CoderListUnit

--- features/coders/src/main/java/com/thindie/coders/presentation/elements/codersList/CoderListUnit.kt
+++ features/coders/src/main/java/com/thindie/coders/presentation/elements/codersList/CoderListUnit.kt
@@ -1,6 +1,7 @@
 package com.thindie.coders.presentation.elements.codersList
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -8,19 +9,22 @@ import androidx.compose.ui.graphics.painter.Painter
 import coil.compose.rememberAsyncImagePainter
 import com.thindie.design_system.KodeTraineeDrawable
 import com.thindie.design_system.elements.KodeTraineeGenericImageComponentRow
-import com.thindie.design_system.elements.KodeTraineeGenericTextContentRow
+import com.thindie.design_system.elements.KodeTraineeGenericTextContentColumn
 import com.thindie.design_system.painter
 import com.thindie.model.coders.CoderModel
 
 @Composable
-internal fun CoderListUnit(coderModel: CoderModel) {
+internal fun CoderListUnit(modifier: Modifier = Modifier, coderModel: CoderModel) {
     val coderAvatar = CoderAsyncAvatarOrStub(coderModel = coderModel)
-    KodeTraineeGenericImageComponentRow(painter = coderAvatar) {
-        KodeTraineeGenericTextContentRow(
+    KodeTraineeGenericImageComponentRow(modifier = modifier, painter = coderAvatar) {
+        KodeTraineeGenericTextContentColumn(
             text = coderModel.getFullName(),
             baseElementTextStyle = MaterialTheme.typography.titleSmall,
             slaveText = coderModel.userTag,
-            slaveElementTextStyle = MaterialTheme.typography.labelSmall
+            slaveElementTextStyle = MaterialTheme.typography.labelSmall,
+            content = {
+                Text(text = coderModel.position)
+            }
         )
     }
 }
@@ -30,7 +34,11 @@ private fun CoderAsyncAvatarOrStub(modifier: Modifier = Modifier, coderModel: Co
 
     val stubGoose = KodeTraineeDrawable.Stub.stubGoose.painter()
 
-    val painter = rememberAsyncImagePainter(model = coderModel.avatarUrl, placeholder = stubGoose)
+    val painter = rememberAsyncImagePainter(
+        model = coderModel.avatarUrl,
+        placeholder = stubGoose,
+        error = stubGoose
+    )
 
     return remember { painter }
 }
