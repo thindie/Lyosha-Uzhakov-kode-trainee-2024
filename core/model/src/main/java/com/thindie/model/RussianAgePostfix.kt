8d61ec1500f8e33b9f5847a1bd3aa3d6021d3d41
Alex <thindie1@mail.ru>
2024-03-13 12:55:13 +0200

    feature coders: model: add CoderProfileModel

---
+++ core/model/src/main/java/com/thindie/model/RussianAgePostfix.kt
@@ -0,0 +1,10 @@
+package com.thindie.model
+
+sealed class RussianAgePostfix {
+    data object One
+    data object TwoToFour
+    data object FiveToTen
+
+    data object Stub
+
+}
---
+++ core/model/src/main/java/com/thindie/model/coder_profile/CoderProfileModel.kt
@@ -0,0 +1,20 @@
+package com.thindie.model.coder_profile
+
+import com.thindie.model.RussianAgePostfix
+
+data class CoderProfileModel(
+    val avatarUrl: String,
+    val firstName: String,
+    val id: String,
+    val lastName: String,
+    val position: String,
+    val userTag: String,
+    val phoneNumber: String,
+    val age: Int,
+    val russianAgePostfix: RussianAgePostfix,
+) {
+    fun getFullName() = firstName
+        .plus(" ")
+        .plus(lastName)
+
+}
